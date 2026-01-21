df3b7c873ac397d672d816f3ab04bec1d25ac108
korn-dev <kasikorntripforwork@gmail.com>
2026-01-21 12:50:00 +0700

    SETUP : format code with biome

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,8 @@ import "@/styles/globals.css";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
-import { TRPCReactProvider } from "@/trpc/react";
 import { cn } from "@/lib/utils";
+import { TRPCReactProvider } from "@/trpc/react";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
